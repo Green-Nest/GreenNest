48b516768baff536fdd9c7ac3041e3e5efaa9103
Vishal22598 <vishalgupta22598@gmail.com>
2025-07-24 02:02:12 +0530

    route added for new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,25 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import About from "./screens/About";
+import AdminPage from "./screens/AdminPage";
+import EditDeleteProduct from "./screens/EditDeleteProduct";
+import ProductManager from "./screens/ProductManager";
+import CustomerManager from "./screens/CustomerManager";
+import OrderManager from "./screens/OrderManager";
+import AdminDashboard from "./screens/AdminDashboard";
 
 export default function App(){
   return <>
   {/* <Navbar /> */}
   <Routes>
+    <Route path="adminPage" element={<AdminPage/>}/>
+    <Route path="editDeleteProduct" element={<EditDeleteProduct/>}/>
+    <Route path="productManager" element={<ProductManager/>}/>
+    <Route path="customerManager" element={<CustomerManager/>}/>
+    <Route path="orderManager" element={<OrderManager/>}/>
+    <Route path="adminDashboard" element={<AdminDashboard/>}/>
     <Route path="/" element={<Home />}>
-
+    
     </Route>
     <Route path="login" element={<Login />}>
       
