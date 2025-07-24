9b004c89d291d757b9e8a15d6d220ef1191d5cdf
PratulSaurabh <saurabh.pratul@gmail.com>
2025-07-24 18:17:46 +0530

    Products Page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,24 +10,27 @@ import IndoorPlants from "./screens/IndoorPlants";
 import OutdoorPlants from './screens/OutdoorPlants';
 import PlantersAndTools from './screens/PlantersAndTools';
 
-// import AdminPage from "./screens/AdminPage";
+import AdminPage from "./screens/AdminPage";
 // import EditDeleteProduct from "./screens/EditDeleteProduct";
-// import ProductManager from "./screens/ProductManager";
-// import CustomerManager from "./screens/CustomerManager";
-// import OrderManager from "./screens/OrderManager";
-// import AdminDashboard from "./screens/AdminDashboard";
+import ProductManager from "./screens/ProductManager";
+import CustomerManager from "./screens/CustomerManager";
+import OrderManager from "./screens/OrderManager";
+import AdminDashboard from "./screens/AdminDashboard";
+import ProductsPage from "./screens/ProductsPage";
 
 export default function App(){
   return <>
-  {/* <Navbar /> */}
+  <Navbar />
   <Routes>
-    {/* <Route path="adminPage" element={<AdminPage/>}/>
-    <Route path="editDeleteProduct" element={<EditDeleteProduct/>}/>
+    <Route path="adminPage" element={<AdminPage/>}/>
+    {/* <Route path="editDeleteProduct" element={<EditDeleteProduct/>}/> */}
     <Route path="productManager" element={<ProductManager/>}/>
     <Route path="customerManager" element={<CustomerManager/>}/>
     <Route path="orderManager" element={<OrderManager/>}/>
-    <Route path="adminDashboard" element={<AdminDashboard/>}/> */}
-    <Route path="/" element={<Home />}>
+    <Route path="adminDashboard" element={<AdminDashboard/>}/>
+    <Route path="productspage" element={<ProductsPage />}/>
+    
+        <Route path="/" element={<Home />}>
     
     </Route>
     <Route path="login" element={<Login />}>
