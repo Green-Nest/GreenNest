11f14920c87d46b3e06cf4915a58fe734c7a81ca
Rishabh Singh <rishu74269@gmail.com>
2025-07-24 19:53:14 +0530

    again pushed data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,22 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import Contact from "./screens/Contact"
 import Register from "./screens/Register";
 import About from "./screens/About";
-<<<<<<< HEAD
-import Contact from "./screens/Contact";
-=======
 import AdminPage from "./screens/AdminPage";
-import EditDeleteProduct from "./screens/EditDeleteProduct";
+
 import ProductManager from "./screens/ProductManager";
 import CustomerManager from "./screens/CustomerManager";
 import OrderManager from "./screens/OrderManager";
 import AdminDashboard from "./screens/AdminDashboard";
->>>>>>> e3862d8aaab7f1a4d69600ad5765c32e78c5bf4b
 
 export default function App(){
   return <>
   {/* <Navbar /> */}
   <Routes>
     <Route path="adminPage" element={<AdminPage/>}/>
-    <Route path="editDeleteProduct" element={<EditDeleteProduct/>}/>
+  
     <Route path="productManager" element={<ProductManager/>}/>
     <Route path="customerManager" element={<CustomerManager/>}/>
     <Route path="orderManager" element={<OrderManager/>}/>
